09bc5fc8070c62a88c5b61968e447a20147d184b
Sandy Carter <sandy.carter@unity3d.com>
2021-02-02 14:35:32 -0500

    Convert Opaque and transparent passes to use RTHandle scaling

--- com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
@@ -345,7 +345,7 @@ namespace UnityEngine.Rendering.Universal.Internal
             return cameraData.requireSrgbConversion && m_EnableSRGBConversionIfNeeded;
         }
 
-        private void Blit(CommandBuffer cmd, RTHandle source, RTHandle destination, Material material, int passIndex = 0)
+        private new void Blit(CommandBuffer cmd, RTHandle source, RTHandle destination, Material material, int passIndex = 0)
         {
             cmd.SetGlobalTexture(ShaderPropertyId.sourceTex, source);
             if (m_UseDrawProcedural)
@@ -362,7 +362,7 @@ namespace UnityEngine.Rendering.Universal.Internal
             }
         }
 
-        private new void Blit(CommandBuffer cmd, RenderTargetIdentifier source, RenderTargetIdentifier destination, Material material, int passIndex = 0)
+        private void Blit(CommandBuffer cmd, RenderTargetIdentifier source, RenderTargetIdentifier destination, Material material, int passIndex = 0)
         {
             cmd.SetGlobalTexture(ShaderPropertyId.sourceTex, source);
             if (m_UseDrawProcedural)
