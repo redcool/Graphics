d4e4c6f1169c577224dabbfe01bad29666e704b4
Alex Vázquez <alex.vazquez@unity3d.com>
2021-04-21 15:47:12 +0200

    [XPipeline][URP]Creating sections on light editor

--- com.unity.render-pipelines.universal/Editor/Lighting/UniversalRenderPipelineLightUI.Drawers.cs
+++ com.unity.render-pipelines.universal/Editor/Lighting/UniversalRenderPipelineLightUI.Drawers.cs
@@ -1,7 +1,9 @@
 namespace UnityEditor.Rendering.Universal
 {
     using UnityEngine;
-    using CED = CoreEditorDrawer<object>;
+    using UnityEngine.Rendering;
+    using UnityEngine.Rendering.Universal;
+    using CED = CoreEditorDrawer<UniversalRenderPipelineSerializedLight>;
 
     internal partial class UniversalRenderPipelineLightUI
     {
@@ -14,7 +16,7 @@ namespace UnityEditor.Rendering.Universal
             Shadows = 1 << 4
         }
 
-        static readonly ExpandedState<Expandable, Light> k_ExpandedState = new ExpandedState<Expandable, Light>(~-1, "URP");
+        static readonly ExpandedState<Expandable, Light> k_ExpandedState = new (~-1, "URP");
 
         public static readonly CED.IDrawer Inspector = CED.Group(
             CED.Conditional(
@@ -36,8 +38,17 @@ namespace UnityEditor.Rendering.Universal
                 k_ExpandedState,
                 DrawGeneralContent),
             CED.Conditional(
-                (serialized, editor) => false,
-                CED.FoldoutGroup(Styles.shapeHeader, Expandable.Shape, k_ExpandedState, DrawShapeContent)),
+                (serializedLight, editor) => !serializedLight.settings.lightType.hasMultipleDifferentValues && serializedLight.settings.light.type == LightType.Spot,
+                CED.FoldoutGroup(Styles.shapeHeader, Expandable.Shape, k_ExpandedState, DrawSpotShapeContent)),
+            CED.Conditional(
+                (serializedLight, editor) =>
+                {
+                    if (serializedLight.settings.lightType.hasMultipleDifferentValues)
+                        return false;
+                    var lightType = serializedLight.settings.light.type;
+                    return lightType == LightType.Rectangle || lightType == LightType.Disc;
+                },
+                CED.FoldoutGroup(Styles.shapeHeader, Expandable.Shape, k_ExpandedState, DrawAreaShapeContent)),
             CED.FoldoutGroup(Styles.emissionHeader,
                 Expandable.Emission,
                 k_ExpandedState,
@@ -53,24 +64,200 @@ namespace UnityEditor.Rendering.Universal
         );
 
 
-        static void DrawGeneralContent(object serialized, Editor owner)
+        static void DrawGeneralContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
         {
+            serializedLight.settings.DrawLightType();
+
+            Light light = serializedLight.settings.light;
+            if (LightType.Directional != light.type && light == RenderSettings.sun)
+            {
+                EditorGUILayout.HelpBox(Styles.SunSourceWarning.text, MessageType.Warning);
+            }
+
+            if (!serializedLight.settings.lightType.hasMultipleDifferentValues)
+            {
+                using (new EditorGUI.DisabledScope(serializedLight.settings.isAreaLightType))
+                    serializedLight.settings.DrawLightmapping();
+
+                if (serializedLight.settings.isAreaLightType && serializedLight.settings.lightmapping.intValue != (int)LightmapBakeType.Baked)
+                {
+                    serializedLight.settings.lightmapping.intValue = (int)LightmapBakeType.Baked;
+                    serializedLight.Apply();
+                }
+            }
         }
 
-        static void DrawShapeContent(object serialized, Editor owner)
+        static void DrawSpotShapeContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
         {
+            serializedLight.settings.DrawInnerAndOuterSpotAngle();
         }
 
-        static void DrawEmissionContent(object serialized, Editor owner)
+        static void DrawAreaShapeContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
         {
+            serializedLight.settings.DrawArea();
         }
 
-        static void DrawRenderingContent(object serialized, Editor owner)
+        static void DrawEmissionContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
         {
+            serializedLight.settings.DrawColor();
+
+            serializedLight.settings.DrawIntensity();
+            serializedLight.settings.DrawBounceIntensity();
+
+            if (!serializedLight.settings.lightType.hasMultipleDifferentValues)
+            {
+                var lightType = serializedLight.settings.light.type;
+                if (lightType == LightType.Directional)
+                {
+#if UNITY_2020_1_OR_NEWER
+                    serializedLight.settings.DrawRange();
+#else
+                serializedLight.settings.DrawRange(false);
+#endif
+                }
+            }
+
+            
         }
 
-        static void DrawShadowsContent(object serialized, Editor owner)
+        static void DrawRenderingContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
         {
+            serializedLight.settings.DrawRenderMode();
+            serializedLight.settings.DrawCullingMask();
+        }
+
+        static void DrawShadowsContent(UniversalRenderPipelineSerializedLight serializedLight, Editor owner)
+        {
+            if (serializedLight.settings.lightType.hasMultipleDifferentValues)
+            {
+                EditorGUILayout.HelpBox("Cannot multi edit shadows from different light types.", MessageType.Info);
+                return;
+            }
+
+            serializedLight.settings.DrawShadowsType();
+
+            if (serializedLight.settings.shadowsType.hasMultipleDifferentValues)
+            {
+                EditorGUILayout.HelpBox("Cannot multi different shadow types", MessageType.Info);
+                return;
+            }
+
+            if (serializedLight.settings.light.shadows == LightShadows.None)
+                return;
+
+            var lightType = serializedLight.settings.light.type;
+
+            using (new EditorGUI.IndentLevelScope())
+            {
+                if (serializedLight.settings.isBakedOrMixed)
+                {
+                    switch (lightType)
+                    {
+                        // Baked Shadow radius
+                        case LightType.Point:
+                        case LightType.Spot:
+                            serializedLight.settings.DrawBakedShadowRadius();
+                            break;
+                        case LightType.Directional:
+                            serializedLight.settings.DrawBakedShadowAngle();
+                            break;
+                    }
+                }
+
+                if (lightType != LightType.Rectangle && !serializedLight.settings.isCompletelyBaked)
+                {
+                    EditorGUILayout.LabelField(Styles.ShadowRealtimeSettings, EditorStyles.boldLabel);
+                    using (new EditorGUI.IndentLevelScope())
+                    {
+                        // Resolution
+                        if (lightType == LightType.Point || lightType == LightType.Spot)
+                            DrawShadowsResolutionGUI(serializedLight);
+
+                        EditorGUILayout.Slider(serializedLight.settings.shadowsStrength, 0f, 1f, Styles.ShadowStrength);
+
+                        // Bias
+                        DrawAdditionalShadowData(serializedLight);
+
+                        // this min bound should match the calculation in SharedLightData::GetNearPlaneMinBound()
+                        float nearPlaneMinBound = Mathf.Min(0.01f * serializedLight.settings.range.floatValue, 0.1f);
+                        EditorGUILayout.Slider(serializedLight.settings.shadowsNearPlane, nearPlaneMinBound, 10.0f, Styles.ShadowNearPlane);
+                    }
+                }
+            }
+
+            if (!UnityEditor.Lightmapping.bakedGI && !serializedLight.settings.lightmapping.hasMultipleDifferentValues && serializedLight.settings.isBakedOrMixed)
+                EditorGUILayout.HelpBox(Styles.BakingWarning.text, MessageType.Warning);
+        }
+
+        static void DrawAdditionalShadowData(UniversalRenderPipelineSerializedLight serializedLight)
+        {
+            // 0: Custom bias - 1: Bias values defined in Pipeline settings
+            int selectedUseAdditionalData = serializedLight.additionalLightData.usePipelineSettings ? 1 : 0;
+            Rect r = EditorGUILayout.GetControlRect(true);
+            EditorGUI.BeginProperty(r, Styles.shadowBias, serializedLight.useAdditionalDataProp);
+            {
+                using (var checkScope = new EditorGUI.ChangeCheckScope())
+                {
+                    selectedUseAdditionalData = EditorGUI.IntPopup(r, Styles.shadowBias, selectedUseAdditionalData, Styles.displayedDefaultOptions, Styles.optionDefaultValues);
+                    if (checkScope.changed)
+                    {
+                        foreach (var additionData in serializedLight.lightsAdditionalData)
+                            additionData.usePipelineSettings = selectedUseAdditionalData != 0;
+
+                        serializedLight.Apply();
+                    }
+                }
+            }
+            EditorGUI.EndProperty();
+
+            if (!serializedLight.useAdditionalDataProp.hasMultipleDifferentValues)
+            {
+                if (selectedUseAdditionalData != 1) // Custom Bias
+                {
+                    using (new EditorGUI.IndentLevelScope())
+                    {
+                        using (var checkScope = new EditorGUI.ChangeCheckScope())
+                        {
+                            EditorGUILayout.Slider(serializedLight.settings.shadowsBias, 0f, 10f, Styles.ShadowDepthBias);
+                            EditorGUILayout.Slider(serializedLight.settings.shadowsNormalBias, 0f, 10f, Styles.ShadowNormalBias);
+                            if (checkScope.changed)
+                                serializedLight.Apply();
+                        }
+                    }
+                }
+            }
+        }
+
+        static void DrawShadowsResolutionGUI(UniversalRenderPipelineSerializedLight serializedLight)
+        {
+            int shadowResolutionTier = serializedLight.additionalLightData.additionalLightsShadowResolutionTier;
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                using (var checkScope = new EditorGUI.ChangeCheckScope())
+                {
+                    Rect r = EditorGUILayout.GetControlRect(true);
+                    r.width += 30;
+
+                    shadowResolutionTier = EditorGUI.IntPopup(r, Styles.ShadowResolution, shadowResolutionTier, Styles.ShadowResolutionDefaultOptions, Styles.ShadowResolutionDefaultValues);
+                    if (shadowResolutionTier == UniversalAdditionalLightData.AdditionalLightsShadowResolutionTierCustom)
+                    {
+                        // show the custom value field GUI.
+                        var newResolution = EditorGUILayout.IntField(serializedLight.settings.shadowsResolution.intValue, GUILayout.ExpandWidth(false));
+                        serializedLight.settings.shadowsResolution.intValue = Mathf.Max(UniversalAdditionalLightData.AdditionalLightsShadowMinimumResolution, Mathf.NextPowerOfTwo(newResolution));
+                    }
+                    else
+                    {
+                        if (GraphicsSettings.renderPipelineAsset is UniversalRenderPipelineAsset urpAsset)
+                            EditorGUILayout.LabelField($"{urpAsset.GetAdditionalLightsShadowResolution(shadowResolutionTier)} ({urpAsset.name})", GUILayout.ExpandWidth(false));
+                    }
+                    if (checkScope.changed)
+                    {
+                        serializedLight.additionalLightsShadowResolutionTierProp.intValue = shadowResolutionTier;
+                        serializedLight.Apply();
+                    }
+                }
+            }
         }
     }
 }
